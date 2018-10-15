82327e6c6380be4361e74f47b44f39c20b211cfd
Andrey Salo <fat.ndy@gmail.com>
2018-10-15 16:46:07 +0300

    initial

---
+++ precache-manifest.208336803b624824b634255b1204ef00.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc6bdaf9d991345a0af4430b8598e371",
+    "url": "/static/media/TrebuchetMS-700-normal.bc6bdaf9.woff"
+  },
+  {
+    "revision": "61a477ade45d9bf57643941308feed20",
+    "url": "/static/media/TrebuchetMS-700-normal.61a477ad.woff2"
+  },
+  {
+    "revision": "6aea92f7e336c8b22550fafb810a3b5e",
+    "url": "/static/media/TrebuchetMS-400-normal.6aea92f7.woff"
+  },
+  {
+    "revision": "89b058476fe7e67710b590294544a249",
+    "url": "/static/media/Exo20-100-normal.89b05847.woff2"
+  },
+  {
+    "revision": "61b7d658bc0f62e3a96ae3bbc79ed4de",
+    "url": "/static/media/Exo20-100-normal.61b7d658.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ffcde178b712601ad4e1",
+    "url": "/static/js/main.ffcde178.chunk.js"
+  },
+  {
+    "revision": "b19ee7007c2486478e80",
+    "url": "/static/js/1.b19ee700.chunk.js"
+  },
+  {
+    "revision": "ffcde178b712601ad4e1",
+    "url": "/static/css/main.ad9f00eb.chunk.css"
+  },
+  {
+    "revision": "0bb56d17f2475bf5e70874efbda9a28c",
+    "url": "/index.html"
+  }
+];
